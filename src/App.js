5923b5f9f23a95e2a8267f8db6f2de3b6fd3f249
Fateme-D <Fateme.Dalirian97@gmail.com>
2022-01-31 13:31:44 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Modal, Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import ModalForm from "./componenet/Modal/Modal";
 
 function App() {
+  const [companies, setCompanies] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [productDetails, setProductDetails] = useState([]);
+  const [show, setShow] = useState(false);
+  const [edit, setEdit] = useState({});
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      const { data } = await axios.get(
+        "http://185.159.179.6/api/Warehouse/GetProductDetails"
+      );
+      setProductDetails(data);
+    };
+    try {
+      fetchProductDetails();
+    } catch (error) {}
+
+    const fetchProducts = async () => {
+      const { data } = await axios.get(
+        "http://185.159.179.6/api/Warehouse/GetProducts"
+      );
+      setProducts(data);
+    };
+    try {
+      fetchProducts();
+    } catch (error) {}
+
+    const fetchCompanies = async () => {
+      const { data } = await axios.get(
+        "http://185.159.179.6/api/Warehouse/GetCompanies"
+      );
+      setCompanies(data);
+    };
+    try {
+      fetchCompanies();
+    } catch (error) {}
+  }, []);
+
+  // useEffect(() => {
+  //   let getCompanies = "http://185.159.179.6/api/Warehouse/GetCompanies";
+  //   let getProducts = "http://185.159.179.6/api/Warehouse/GetProducts";
+  //   let getProductDetails =
+  //     "http://185.159.179.6/api/Warehouse/getProductDetails";
+
+  //   const requestOne = axios.get(getCompanies);
+  //   const requestTwo = axios.get(getProducts);
+  //   const requestThree = axios.get(getProductDetails);
+
+  //   axios
+  //     .all([requestOne, requestTwo, requestThree])
+  //     .then(
+  //       axios.spread((...responses) => {
+  //         const resGetCompanies = responses[0];
+  //         const resGetProducts = responses[1];
+  //         const resGetProductDetails = responses[2];
+
+  //         setCompanies(resGetCompanies.data);
+  //         setProducts(resGetProducts.data);
+  //         setProductDetails(resGetProductDetails.data);
+  //       })
+  //     )
+  //     .catch((errors) => {});
+  // }, []);
+
+  const handleClose = () => setShow(false);
+  const onModalFormSubmit = (e) => {
+    e.preventDefault();
+    handleClose();
+  };
+
+  const editProductHandler = (id) => {
+    setShow(true);
+    const index = productDetails.findIndex((p) => p.id === id);
+    const selectedProduct = { ...productDetails[index] };
+  };
+
+  const modalHandler = (id) => {
+    const index = productDetails.findIndex((p) => p.id === id);
+    const selectedProduct = { ...productDetails[index] };
+    setEdit(selectedProduct);
+    setShow(true);
+  };
+
+  function sortRowData() {
+    const newData = {};
+    for (const product of products) {
+      const sameProductsId = productDetails
+        .sort((a, b) => a.companyId - b.companyId)
+        .filter((pD) => pD.productId === product.id);
+      newData[product["title"]] = sameProductsId;
+    }
+    return newData;
+  }
+  const orderedData = sortRowData();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tableContainer">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th></th>
+              {companies
+                .sort((a, b) => a.id - b.id)
+                .map((c) => (
+                  <th>{c.title}</th>
+                ))}
+            </tr>
+          </thead>
+          <tbody>
+            {[...Array(companies.length).keys()].map((i, index) => {
+              return (
+                <tr key={i.id}>
+                  <td key={i.id}>{Object.keys(orderedData)[index]}</td>
+                  {orderedData[Object.keys(orderedData)[index]].map((d) => {
+                    return (
+                      <td key={d.id} onClick={() => modalHandler(d.id)}>
+                        id:{d.id} - pId:{d.productId} - cId:{d.companyId}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal Form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ModalForm
+            onSubmit={onModalFormSubmit}
+            editProductHandler={editProductHandler}
+            edit={edit}
+          />
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
---
+++ src/componenet/Modal/Modal.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+const ModalForm = ({ onSubmit, edit }) => {
+  const [input, setInput] = useState({ price: "", quantity: "" });
+
+  useEffect(() => {
+    setInput({ ...input, price: edit.price, quantity: edit.quantity });
+  }, [edit.price, edit.quantity, input]);
+
+  const changeHandler = (e) => {
+    setInput(([e.target.name] = e.target.value));
+  };
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group>
+        <Form.Label>Price</Form.Label>
+        <Form.Control
+          type="tel"
+          placeholder="Enter Price"
+          value={input.price}
+          name="price"
+          onChange={(e) => changeHandler(e)}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label style={{ marginTop: "15px" }}>Quantity</Form.Label>
+        <Form.Control
+          type="tel"
+          placeholder="Enter Quantity"
+          value={input.quantity}
+          name="price"
+          onChange={(e) => changeHandler(e)}
+        />
+      </Form.Group>
+      <Button
+        variant="primary"
+        type="submit"
+        block
+        style={{ marginTop: "15px" }}
+      >
+        submit
+      </Button>
+    </Form>
+  );
+};
+export default ModalForm;
